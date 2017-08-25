c8b8a99fbd3228eff4d3954954fd707e32b0a883
Eugene Papkou <30703998+EugenePapkou@users.noreply.github.com>
2017-08-25 13:16:26 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,11 +10,13 @@ public class Main
         int a = Integer.parseInt(scanner.nextLine());//2
         int b = Integer.parseInt(scanner.nextLine());//3
         int c = Integer.parseInt(scanner.nextLine());//1
-        Equation e = new QuadEquation(a, b, c); // here're working both of logical blocks (static and non-static from class Equation)
+        Equation e = new QuadEquation(a, b, c); // here're working both of logical blocks (static and non-static 
+	    					// from class Equation)
         
         e.solve();        
         //e.print();
-        System.out.println(e); // here will be printed the string which returns the overriden method toString() (basic method of class Object)
+        System.out.println(e); // here will be printed the string which returns the overriden method toString() 
+	    		       // (basic method of class Object)
         
         
         // e = new Equation(); - it's prohibited because of the class is abstract
